c916a39ce9f84e5d96bb2f888a323a52f7d358f6
dlo137 <dangelo.watson1212@gmail.com>
2022-11-08 13:38:01 -0500

    Start of list

---
+++ src/components/list/List.jsx
@@ -0,0 +1,9 @@
+import './list.scss'
+
+export default function list() {
+  return (
+    <div clasName = 'list'>
+        list
+    </div>
+  )
+}
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,12 +1,18 @@
 import Navbar from "../components/navbar/Navbar"
 import Featured from "../components/featured/Featured"
 import "./home.scss"
+import List from "../components/list/List"
 
 const Home = () => {
   return (
     <div className = "home">
       <Navbar/>
       <Featured type="movie"/>
+      <List />
+      <List />
+      <List />
+      <List />
+
     </div>
   )
 }
